7a3f1f67a12e26e5a10a487d4b02fd506101c0d0
afhan-akhtar <affanlodhi333@gmail.com>
2025-08-14 10:26:51 +0500

    comment out the gsap img-1,2,3

--- script.js
+++ script.js
@@ -111,169 +111,169 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   // scale img-1
-  gsap.to(".img-1 img", {
-    scale: 1.125,
-    ease: "none",
-    scrollTrigger: {
-      trigger: stickySection,
-      start: "top top",
-      end: () => `+=${window.innerHeight}`,
-      scrub: true,
-    },
-  });
+  // gsap.to(".img-1 img", {
+  //   scale: 1.125,
+  //   ease: "none",
+  //   scrollTrigger: {
+  //     trigger: stickySection,
+  //     start: "top top",
+  //     end: () => `+=${window.innerHeight}`,
+  //     scrub: true,
+  //   },
+  // });
 
-  // animate img-2's clip-path
-  gsap.to(".img-2", {
-    clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
-    ease: "none",
-    scrollTrigger: {
-      trigger: stickySection,
-      start: "top top",
-      end: () => `+=${window.innerHeight}`,
-      scrub: true,
-      onUpdate: (self) => {
-        const progress = self.progress;
-        gsap.set(".img-2", {
-          clipPath: `polygon(
-        ${gsap.utils.interpolate(40, 0, progress)}% ${gsap.utils.interpolate(
-            25,
-            0,
-            progress
-          )}%,
-        ${gsap.utils.interpolate(60, 100, progress)}% ${gsap.utils.interpolate(
-            25,
-            0,
-            progress
-          )}%,
-        ${gsap.utils.interpolate(60, 100, progress)}% ${gsap.utils.interpolate(
-            75,
-            100,
-            progress
-          )}%,
-        ${gsap.utils.interpolate(40, 0, progress)}% ${gsap.utils.interpolate(
-            75,
-            100,
-            progress
-          )}%
-      )`,
-        });
-      },
-    },
-  });
+  // // animate img-2's clip-path
+  // gsap.to(".img-2", {
+  //   clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+  //   ease: "none",
+  //   scrollTrigger: {
+  //     trigger: stickySection,
+  //     start: "top top",
+  //     end: () => `+=${window.innerHeight}`,
+  //     scrub: true,
+  //     onUpdate: (self) => {
+  //       const progress = self.progress;
+  //       gsap.set(".img-2", {
+  //         clipPath: `polygon(
+  //       ${gsap.utils.interpolate(40, 0, progress)}% ${gsap.utils.interpolate(
+  //           25,
+  //           0,
+  //           progress
+  //         )}%,
+  //       ${gsap.utils.interpolate(60, 100, progress)}% ${gsap.utils.interpolate(
+  //           25,
+  //           0,
+  //           progress
+  //         )}%,
+  //       ${gsap.utils.interpolate(60, 100, progress)}% ${gsap.utils.interpolate(
+  //           75,
+  //           100,
+  //           progress
+  //         )}%,
+  //       ${gsap.utils.interpolate(40, 0, progress)}% ${gsap.utils.interpolate(
+  //           75,
+  //           100,
+  //           progress
+  //         )}%
+  //     )`,
+  //       });
+  //     },
+  //   },
+  // });
 
-  // spinning effect on img-2
-  gsap.to(".img-2 img", {
-    scale: 1.125,
-    rotation: 360,
-    ease: "none",
-    scrollTrigger: {
-      trigger: stickySection,
-      start: "top top",
-      end: () => `+=${window.innerHeight}`,
-      scrub: true,
-    },
-  });
+  // // spinning effect on img-2
+  // gsap.to(".img-2 img", {
+  //   scale: 1.125,
+  //   rotation: 360,
+  //   ease: "none",
+  //   scrollTrigger: {
+  //     trigger: stickySection,
+  //     start: "top top",
+  //     end: () => `+=${window.innerHeight}`,
+  //     scrub: true,
+  //   },
+  // });
 
-  // animate img-3's clip-path
-  gsap.to(".img-3", {
-    clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
-    ease: "none",
-    scrollTrigger: {
-      trigger: stickySection,
-      start: () => `${window.innerHeight * 3}`,
-      end: () => `${window.innerHeight * 4}`,
-      scrub: true,
-      onUpdate: (self) => {
-        const progress = self.progress;
-        gsap.set(".img-3", {
-          clipPath: `polygon(
-          ${gsap.utils.interpolate(50, 0, progress)}% ${gsap.utils.interpolate(
-            50,
-            0,
-            progress
-          )}%,
-          ${gsap.utils.interpolate(
-            50,
-            100,
-            progress
-          )}% ${gsap.utils.interpolate(50, 0, progress)}%,
-          ${gsap.utils.interpolate(
-            50,
-            100,
-            progress
-          )}% ${gsap.utils.interpolate(50, 100, progress)}%,
-          ${gsap.utils.interpolate(50, 0, progress)}% ${gsap.utils.interpolate(
-            50,
-            100,
-            progress
-          )}%
-        )`,
-        });
-      },
-    },
-  });
+  // // animate img-3's clip-path
+  // gsap.to(".img-3", {
+  //   clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+  //   ease: "none",
+  //   scrollTrigger: {
+  //     trigger: stickySection,
+  //     start: () => `${window.innerHeight * 3}`,
+  //     end: () => `${window.innerHeight * 4}`,
+  //     scrub: true,
+  //     onUpdate: (self) => {
+  //       const progress = self.progress;
+  //       gsap.set(".img-3", {
+  //         clipPath: `polygon(
+  //         ${gsap.utils.interpolate(50, 0, progress)}% ${gsap.utils.interpolate(
+  //           50,
+  //           0,
+  //           progress
+  //         )}%,
+  //         ${gsap.utils.interpolate(
+  //           50,
+  //           100,
+  //           progress
+  //         )}% ${gsap.utils.interpolate(50, 0, progress)}%,
+  //         ${gsap.utils.interpolate(
+  //           50,
+  //           100,
+  //           progress
+  //         )}% ${gsap.utils.interpolate(50, 100, progress)}%,
+  //         ${gsap.utils.interpolate(50, 0, progress)}% ${gsap.utils.interpolate(
+  //           50,
+  //           100,
+  //           progress
+  //         )}%
+  //       )`,
+  //       });
+  //     },
+  //   },
+  // });
 
-  // continue img-2's scale
-  gsap.fromTo(
-    ".img-2 img",
-    { scale: 1.125 },
-    {
-      scale: 1.25,
-      rotation: 360,
-      ease: "none",
-      scrollTrigger: {
-        trigger: stickySection,
-        start: () => `${window.innerHeight * 3}`,
-        end: () => `${window.innerHeight * 4}`,
-        scrub: true,
-      },
-    }
-  );
+  // // continue img-2's scale
+  // gsap.fromTo(
+  //   ".img-2 img",
+  //   { scale: 1.125 },
+  //   {
+  //     scale: 1.25,
+  //     rotation: 360,
+  //     ease: "none",
+  //     scrollTrigger: {
+  //       trigger: stickySection,
+  //       start: () => `${window.innerHeight * 3}`,
+  //       end: () => `${window.innerHeight * 4}`,
+  //       scrub: true,
+  //     },
+  //   }
+  // );
 
-  // scale img-3
-  gsap.to(".img-3 img", {
-    scale: 2.9,
-    ease: "none",
-    scrollTrigger: {
-      trigger: stickySection,
-      start: () => `${window.innerHeight * 3}`,
-      end: () => `${window.innerHeight * 4}`,
-      scrub: true,
-    },
-  });
+  // // scale img-3
+  // gsap.to(".img-3 img", {
+  //   scale: 2.9,
+  //   ease: "none",
+  //   scrollTrigger: {
+  //     trigger: stickySection,
+  //     start: () => `${window.innerHeight * 3}`,
+  //     end: () => `${window.innerHeight * 4}`,
+  //     scrub: true,
+  //   },
+  // });
 
-  // reset img-3's scale
-  gsap.fromTo(
-    ".img-3 img",
-    { scale: 2.9 },
-    {
-      scale: 1,
-      ease: "none",
-      scrollTrigger: {
-        trigger: stickySection,
-        start: () => `${window.innerHeight * 4}`,
-        end: () => `${window.innerHeight * 6}`,
-        scrub: true,
-      },
-    }
-  );
+  // // reset img-3's scale
+  // gsap.fromTo(
+  //   ".img-3 img",
+  //   { scale: 2.9 },
+  //   {
+  //     scale: 1,
+  //     ease: "none",
+  //     scrollTrigger: {
+  //       trigger: stickySection,
+  //       start: () => `${window.innerHeight * 4}`,
+  //       end: () => `${window.innerHeight * 6}`,
+  //       scrub: true,
+  //     },
+  //   }
+  // );
 
   // final copy reveal
-  let tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: stickySection,
-      start: () => `${window.innerHeight * 4.5}`,
-      end: () => `${window.innerHeight * 5.5}`,
-      scrub: true,
-      toggleActions: "play reverse play reverse",
-    },
-  });
-  tl.to(".copy", {
-    display: "block",
-    rotateY: 0,
-    scale: 1,
-    duration: 1,
-  });
+  // let tl = gsap.timeline({
+  //   scrollTrigger: {
+  //     trigger: stickySection,
+  //     start: () => `${window.innerHeight * 4.5}`,
+  //     end: () => `${window.innerHeight * 5.5}`,
+  //     scrub: true,
+  //     toggleActions: "play reverse play reverse",
+  //   },
+  // });
+  // tl.to(".copy", {
+  //   display: "block",
+  //   rotateY: 0,
+  //   scale: 1,
+  //   duration: 1,
+  // });
 
   // --- Minimalist SVG Animation on the first brand-splash ---
   try {
